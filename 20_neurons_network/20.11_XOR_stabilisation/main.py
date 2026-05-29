d85ed4842656b305b627f1d21e48981bec6bf3b8
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-05-29 11:43:42 +0200

    ajout du projet 20.11_XOR_stabilisation + premier forward

---
+++ 20_neurons_network/20.11_XOR_stabilisation/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+x = np.array([[0, 0],
+              [1, 0],
+              [1, 1],
+              [0, 1]])
+
+y_true = np.array([0, 1, 0, 1])
+
+w1 = np.random.rand(2) - 0.5
+b1 = np.random.rand() - 0.5
+
+w2 = np.random.rand(2) - 0.5
+b2 = np.random.rand() - 0.5
+
+w_final = np.random.rand(2) - 0.5
+b_final = np.random.rand() - 0.5
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def sigmoid_derivative(s):
+    return s * (1 - s)
+
+def neuron(dataset, weights, bias):
+    score = np.dot(dataset, weights) + bias
+    y_pred = sigmoid(score)
+    return y_pred
+
+output_neuron1 = neuron(x, w1, b1)
+output_neuron2 = neuron(x, w2, b2)
+final_dataset = np.array([output_neuron1, output_neuron2]).T
+output_final_neuron = neuron(final_dataset, w_final, b_final)
