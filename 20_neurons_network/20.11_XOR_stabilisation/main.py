c1ce9a4a6c92fb9b6fb9a6d05248ac3dc2412015
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-05-29 11:54:46 +0200

    première retropropagation naïve du neurone final

--- 20_neurons_network/20.11_XOR_stabilisation/main.py
+++ 20_neurons_network/20.11_XOR_stabilisation/main.py
@@ -16,6 +16,8 @@ b2 = np.random.rand() - 0.5
 w_final = np.random.rand(2) - 0.5
 b_final = np.random.rand() - 0.5
 
+lr = 0.1
+
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
@@ -30,4 +32,11 @@ def neuron(dataset, weights, bias):
 output_neuron1 = neuron(x, w1, b1)
 output_neuron2 = neuron(x, w2, b2)
 final_dataset = np.array([output_neuron1, output_neuron2]).T
+final_output = neuron(final_dataset, w_final, b_final)
+error = final_output - y_true
+cost = np.mean(error ** 2)
+dw_final = np.dot(final_dataset.T, error) / len(final_dataset)
+db_final = np.mean(error)
+w_final = w_final - lr * dw_final
+b_final = b_final - lr * db_final
 output_final_neuron = neuron(final_dataset, w_final, b_final)
