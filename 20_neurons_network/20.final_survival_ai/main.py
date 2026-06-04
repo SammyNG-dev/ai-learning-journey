1f659bc5eb57bb82b7b2dee487ffa365c34a3421
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2026-06-04 15:16:34 +0200

    retropropagation de la 1ere couche de neurones

--- 20_neurons_network/20.final_survival_ai/main.py
+++ 20_neurons_network/20.final_survival_ai/main.py
@@ -32,7 +32,7 @@ y_train = np.array([
     0
 ])
 
-lr = 0.1
+lr = 1
 
 weights1 = np.random.rand(6, 4) - 0.5
 bias1 = np.random.rand(4) - 0.5
@@ -54,30 +54,47 @@ def neuron(dataset, weights, bias):
     y_pred = sigmoid(score)
     return y_pred
 
-# forward
-
-output_hidden_neurons_layer1 = neuron(x_train, weights1, bias1)
-output_hidden_neurons_layer2 = neuron(output_hidden_neurons_layer1, weights2, bias2)
-output_final_neuron = neuron(output_hidden_neurons_layer2, weights_final, bias_final)
 y_train = y_train.reshape(-1, 1)
 
-# calcul de l'erreur, du coût et du delta
+# boucle d'apprentissage
 
-error = output_final_neuron - y_train
-cost = np.mean(error ** 2)
-final_delta = error * sigmoid_derivative(output_final_neuron)
+for i in range(1000):
 
-#retropropagation de la 2eme couche de neurones
+    # forward
 
-delta_hidden_neurons_layer2 = np.dot(final_delta, weights_final.T) * sigmoid_derivative(output_hidden_neurons_layer2)
-dw2 = np.dot(output_hidden_neurons_layer1.T, delta_hidden_neurons_layer2) / len(output_hidden_neurons_layer1)
-db2 = np.mean(delta_hidden_neurons_layer2, axis=0)
-weights2 = weights2 - lr * dw2
-bias2 = bias2 - lr * db2
+    output_hidden_neurons_layer1 = neuron(x_train, weights1, bias1)
+    output_hidden_neurons_layer2 = neuron(output_hidden_neurons_layer1, weights2, bias2)
+    output_final_neuron = neuron(output_hidden_neurons_layer2, weights_final, bias_final)
 
-# retropropagation du neurone final
+    # calcul de l'erreur, du coût et des deltas
 
-dw_final = np.dot(output_hidden_neurons_layer2.T, final_delta) / len(x_train)
-db_final = np.mean(final_delta, axis=0)
-weights_final = weights_final - lr * dw_final
-bias_final = bias_final - lr * db_final
+    error = output_final_neuron - y_train
+    cost = np.mean(error ** 2)
+    if i % 100 == 0:
+        print(f"iteration {i}:")
+        print(f"cost: {cost}")
+    final_delta = error * sigmoid_derivative(output_final_neuron)
+    delta_hidden_neurons_layer2 = np.dot(final_delta, weights_final.T) * sigmoid_derivative(output_hidden_neurons_layer2)
+    delta_hidden_neurons_layer1 = np.dot(delta_hidden_neurons_layer2, weights2.T) * sigmoid_derivative(output_hidden_neurons_layer1)
+
+    # calcul des gradients et retropropagation de la 1ere couche de neurones (6 entrées - 4 neurones)
+
+    dw1 = np.dot(x_train.T, delta_hidden_neurons_layer1) / len(x_train)
+    db1 = np.mean(delta_hidden_neurons_layer1, axis=0)
+    weights1 = weights1 - lr * dw1
+    bias1 = bias1 - lr * db1
+
+    # calcul des gradients et retropropagation de la 2e couche de neurones (4 entrées - 2 neurones)
+
+    dw2 = np.dot(output_hidden_neurons_layer1.T, delta_hidden_neurons_layer2) / len(output_hidden_neurons_layer1)
+    db2 = np.mean(delta_hidden_neurons_layer2, axis=0)
+    weights2 = weights2 - lr * dw2
+    bias2 = bias2 - lr * db2
+
+    # calcul des gradients retropropagation du neurone final
+
+    dw_final = np.dot(output_hidden_neurons_layer2.T, final_delta) / len(x_train)
+    db_final = np.mean(final_delta, axis=0)
+    weights_final = weights_final - lr * dw_final
+    bias_final = bias_final - lr * db_final
+
